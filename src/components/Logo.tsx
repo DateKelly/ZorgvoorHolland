359f93eaaf2658a3a6105994c537415d36eddfb5
Venson Estrope <vensonestrope13@gmail.com>
2025-02-10 06:17:06 +0800

    Fixed Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from "../assets/logo.jpg";
 
-export const Logo = ({ className = "w-8 h-8" }: { className?: string }) => (
+export const Logo = ({ className="w-40" }: { className?: string }) => (
     <img src={logo} alt="Logo" className={className} />
 );
